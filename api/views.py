e663b8fea435668e84fcd40109b93216926b1ff7
kaiqueBellmont <kaiquebellmont@gmail.com>
2023-05-01 17:36:03 -0300

    integration with OpenAI worker

--- api/views.py
+++ api/views.py
@@ -1,7 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from api.analizer.code_analizer import AnalizeCode
-import requests
+from .requester import requester
+
 
 class CodeAnalizer(APIView):
     """
@@ -12,15 +13,33 @@ class CodeAnalizer(APIView):
         """
         :param request:
         """
-        req = requests.get("http://0.0.0.0:9000/")
-        req = req.json()
-        print(req)
-        return Response(req)
+        req = requester.GitHookApiRequester()
+        res = req.request.get(req.params.OPEN_AI_WORKER_ENDPOINT)
+
+        return Response(res.json())
 
     def post(self, request):
         """
         :param request:
         :return:
         """
+        req = requester.GitHookApiRequester()
+
+        open_ai_insight_response = req.request.post(
+            url=req.params.OPEN_AI_WORKER_ENDPOINT+"insight",
+            json=request.data
+        )
+
         data = AnalizeCode.analyze(request.data)
-        return Response(data)
+
+        if open_ai_insight_response.status_code == 200:
+            result = open_ai_insight_response.json()
+            # Processar o resultado retornado
+            print(result)
+            print(data)
+
+            result = {"code": data, "insight": open_ai_insight_response}
+            return Response(result)
+        else:
+            print("Erro na solicitação:", open_ai_insight_response.status_code, open_ai_insight_response.text)
+            return Response("Erro na solicitação:", open_ai_insight_response.status_code, open_ai_insight_response.text)
